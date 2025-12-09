8f91efd9a8d654666473ff6f7f54970f3ed2bf4d
Artem Andreev <artem.andreev@ringcentral.com>
2025-12-09 22:30:56 +0400

    Initial Commit

---
+++ TeleprompterPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TeleprompterPro
+//
+//  Created by Артем Андреев on 09.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TeleprompterPro/TeleprompterProApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeleprompterProApp.swift
+//  TeleprompterPro
+//
+//  Created by Артем Андреев on 09.12.25.
+//
+
+import SwiftUI
+
+@main
+struct TeleprompterProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
